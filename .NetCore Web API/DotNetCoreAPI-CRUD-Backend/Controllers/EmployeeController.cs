4cb1f73ab2bd39f3f9761b68e598432f784e0b1e
DarshanBoghra <72594880+DarshanBoghra@users.noreply.github.com>
2025-08-15 17:34:46 +0000

    .NetCore Web API

---
+++ .NetCore Web API/DotNetCoreAPI-CRUD-Backend/Controllers/EmployeeController.cs
@@ -0,0 +1,102 @@
+﻿using DotNetCoreAPI_CRUD_Backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreAPI_CRUD_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _employeeContext;
+        public EmployeeController(EmployeeContext employeeContext) 
+        {
+            _employeeContext = employeeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            if(_employeeContext == null)
+            {
+                return NotFound();
+            }
+            return await _employeeContext.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            if (_employeeContext.Employees == null)
+            {
+                return NotFound();
+            }
+
+            var emp = await _employeeContext.Employees.FindAsync(id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return emp;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+            _employeeContext.Employees.Add(employee);
+            await _employeeContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetEmployee), new {id = employee.ID}, employee);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Employee>> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return BadRequest();
+            }
+            _employeeContext.Entry(employee).State = EntityState.Modified;
+            try
+            {
+                await _employeeContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!IsEmployeeAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok();
+        }
+
+        private bool IsEmployeeAvailable(int id) 
+        {
+            return (_employeeContext.Employees?.Any(x => x.ID == id)).GetValueOrDefault();    
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteBrand(int id)
+        {
+            if (_employeeContext.Employees == null)
+            {
+                return NotFound();
+            }
+            var brand = await _employeeContext.Employees.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+            _employeeContext.Employees.Remove(brand);
+            await _employeeContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ .NetCore Web API/DotNetCoreAPI-CRUD-Backend/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreAPI_CRUD_Backend.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
